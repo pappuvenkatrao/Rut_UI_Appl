d3f639a39690a8542ef1cd185b2c2b06bc01d4de
Venkat Rao Pappu <pappuvenkatrao@live.com>
2017-07-13 18:50:40 +0530

    FirstComit

---
+++ Rut/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rut.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult DBDesigner()
+        {
+            return View();
+        }
+    }
+}
---
+++ Rut/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rut.Startup))]
+namespace Rut
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
